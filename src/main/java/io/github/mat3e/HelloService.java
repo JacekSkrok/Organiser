8a31ec1429498cb2f9a5e21cd124c5e69088280f
LazyJackyboy <skrok.jacek21@gmail.com>
2022-11-28 18:22:20 +0100

    Added two test of HelloService

--- src/main/java/io/github/mat3e/HelloService.java
+++ src/main/java/io/github/mat3e/HelloService.java
@@ -6,6 +6,6 @@ class HelloService {
     static final String FALLBACK_NAME = "world";
 
     String prepareGreeting(String name) {
-        return "Hello" + Optional.ofNullable(name).orElse(FALLBACK_NAME);
+        return "Hello " + Optional.ofNullable(name).orElse(FALLBACK_NAME);
     }
 }
---
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -0,0 +1,31 @@
+package io.github.mat3e;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class HelloServiceTest {
+    private HelloService SUT = new HelloService();
+    @Test
+    public void test_prepareGreeting_null_returnsGreetingWithFallbackValue()  {
+        //given + when
+        var result = SUT.prepareGreeting(null);
+
+        //then
+        assertEquals("Hello " + HelloService.FALLBACK_NAME, result);
+    }
+
+    @Test
+    public void test_prepareGreeting_name_returnsGreetingWithName()  {
+        //given + when
+
+        String name = "testname";
+
+        //when
+        var result = SUT.prepareGreeting(name);
+
+        //then
+        assertEquals("Hello " + name , result);
+    }
+}
