060a91322b8972600abfe5c598c823220749f4ce
LazyJackyboy <skrok.jacek21@gmail.com>
2022-12-11 21:23:33 +0100

    added h2 and flyway with 2 migrations

---
+++ src/main/resources/db/migration/V1__Create_languages_table.sql
@@ -0,0 +1,5 @@
+create table LANGUAGES (
+    ID int unsigned primary key auto_increment,
+    WELCOME_MSG varchar(100) not null,
+    code varchar(3)
+);
---
+++ src/main/resources/db/migration/V2__Add_languages.sql
@@ -0,0 +1,2 @@
+insert into LANGUAGES (WELCOME_MSG, code) values ('Hello', 'en');
+insert into LANGUAGES (WELCOME_MSG, code) values ('Witaj', 'pl');
