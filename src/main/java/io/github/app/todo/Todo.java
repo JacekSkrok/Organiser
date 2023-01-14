2d6fbbc787826b9964cd6382eb6c589c1bed6b75
LazyJackyboy <skrok.jacek21@gmail.com>
2023-01-14 16:35:44 +0100

    added logic for todos, class, repository and servlet, tested this endpoint

---
+++ src/main/java/io/github/app/todo/Todo.java
@@ -0,0 +1,49 @@
+package io.github.app.todo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TODOS")
+class Todo {
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    private Integer id;
+    private String content;
+    private boolean status;
+
+    /**
+     *  For Hibernate(JPA) usage
+     */
+    @SuppressWarnings("unused")
+    public Todo() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/io/github/app/todo/TodoRepository.java
@@ -0,0 +1,31 @@
+package io.github.app.todo;
+
+import io.github.app.HibernateUtil;
+import io.github.app.lang.Lang;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TodoRepository {
+    List<Todo> findAll() {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.createQuery("from Todo", Todo.class).list();
+
+        transaction.commit();
+        session.close();
+        return result;
+
+    }
+    public Optional<Lang> findByID(Integer id) {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = Optional.ofNullable(session.get(Lang.class, id));
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
+}
